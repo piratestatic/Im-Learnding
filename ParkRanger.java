f2abdc73031f2c8be609c172fcba0d306fb344f4
piratestatic <coreyrhodges@gmail.com>
2020-05-07 20:43:40 -1000

    Minor changes

--- ParkRanger.java
+++ ParkRanger.java
@@ -1,3 +1,9 @@
+/*
+File: ParkRanger.java (HW4)
+* Author: Corey Hodges
+* Date: May 2, 2020
+* Using a 2D array to print values to the screen
+*/
 import java.util.*;
 import java.lang.*;
 
@@ -13,22 +19,30 @@ class ParkRanger {
         { "Connecticut", "Robin", "Mountain Laurel", "false" },
         { "Delaware", "Blue Hen Chicken", "Peach Blossom", "false" },
         { "Florida", "Mockingbird", "Orange Blossom", "false" },
-        { "Georgia", "Brown Thrasher", "Cherokee Rose", "false" }, { "Hawaii", "Nene", "Hawaiian Hibiscus", "false" },
-        { "Idaho", "Mountain Bluebird", "Syringa, mock orange", "false" }, { "Illinois", "Cardinal", "Violet", "false" },
-        { "Indiana", "Cardinal", "Peony", "false" }, { "Iowa", "Eastern Goldfinch", "Wild Praire Rose", "false" },
-        { "Kansas", "Western Meadowlark", "Sunflower", "false" }, { "Kentucky", "Cardinal", "Goldenrod", "false" },
+        { "Georgia", "Brown Thrasher", "Cherokee Rose", "false" },
+        { "Hawaii", "Nene", "Hawaiian Hibiscus", "false" },
+        { "Idaho", "Mountain Bluebird", "Syringa, mock orange", "false" },
+        { "Illinois", "Cardinal", "Violet", "false" },
+        { "Indiana", "Cardinal", "Peony", "false" },
+        { "Iowa", "Eastern Goldfinch", "Wild Praire Rose", "false" },
+        { "Kansas", "Western Meadowlark", "Sunflower", "false" },
+        { "Kentucky", "Cardinal", "Goldenrod", "false" },
         { "Louisiana", "Eastern Brown Pelican", "Magnolia", "false" },
         { "Maine", "Chickadee", "Pine Cone and Tassel", "false" },
         { "Maryland", "Baltimore Oriole", "Black-Eyed Susan", "false" },
-        { "Massachusetts", "Chickadee", "Mayflower", "false" }, { "Michigan", "Robin", "Apple Blossom", "false" },
+        { "Massachusetts", "Chickadee", "Mayflower", "false" },
+        { "Michigan", "Robin", "Apple Blossom", "false" },
         { "Minnesota", "Common Loon", "Pink and White Lady's Slippper", "false" },
-        { "Mississippi", "Mockingbird", "Magnolia", "false" }, { "Missouri", "Bluebird", "Hawthorn", "false" },
+        { "Mississippi", "Mockingbird", "Magnolia", "false" },
+        { "Missouri", "Bluebird", "Hawthorn", "false" },
         { "Montana", "Western Meadowlark", "Bitterroot", "false" },
         { "Nebraska", "Western Meadowlark", "Goldenrod", "false" },
         { "Nevada", "Mountain Bluebird", "Sagebrush", "false" },
         { "New Hampshire", "Purple Finch", "Purple Lilac", "false" },
-        { "New Jersey", "Eastern Goldfinch", "Violet", "false" }, { "New Mexico", "Roadrunner", "Yucca Flower", "false" },
-        { "New York", "Bluebird", "Rose", "false" }, { "North Carolina", "Cardinal", "Flowering Dogwood", "false" },
+        { "New Jersey", "Eastern Goldfinch", "Violet", "false" },
+        { "New Mexico", "Roadrunner", "Yucca Flower", "false" },
+        { "New York", "Bluebird", "Rose", "false" },
+        { "North Carolina", "Cardinal", "Flowering Dogwood", "false" },
         { "North Dakota", "Western Meadowlark", "Wild Praire Rose", "false" },
         { "Ohio", "Cardinal", "Scarlet Carnation", "false" },
         { "Oklahoma", "Scissor-tailed Flycatcher", "Oklahoma Rose", "false" },
@@ -37,11 +51,14 @@ class ParkRanger {
         { "Rhode Island", "Rhode Island Red", "Violet", "false" },
         { "South Carolina", "Great Carolina Wren", "Yellow Jessamine", "false" },
         { "South Dakota", "Ring-necked Pheasant", "Pasque Flower", "false" },
-        { "Tennessee", "Mockingbird", "Purple Passionflower", "false" },
-        { "Texas", "Mockingbird", "Bluebonnet Sp.", "false" }, { "Utah", "Common American Gull", "Sego Lily", "false" },
-        { "Vermont", "Hermit Thrush", "Red Clover", "false" }, { "Virginia", "Cardinal", " American Dogwood", "false" },
+        { "Tennessee", "Mockingbird", "Iris", "false" },
+        { "Texas", "Mockingbird", "Bluebonnet Sp.", "false" },
+        { "Utah", "Common American Gull", "Sego Lily", "false" },
+        { "Vermont", "Hermit Thrush", "Red Clover", "false" },
+        { "Virginia", "Cardinal", " American Dogwood", "false" },
         { "Washington", "Willow Goldfinch", "Coast Rhododendrum", "false" },
-        { "West Virginia", "Cardinal", "Rhododendron", "false" }, { "Wisconsin", "Robin", "Wood Violet", "false" },
+        { "West Virginia", "Cardinal", "Rhododendron", "false" },
+        { "Wisconsin", "Robin", "Wood Violet", "false" },
         { "Wyoming", "Western Meadowlark", "Indian Paintbrush", "false" } };
         //2D array of all 50 states, state bird and flower
   public static void main(String[] args) {
