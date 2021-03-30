eda180d0a5d9a572e9e107730d39fc6349402674
Pirate Static <coreyrhodges@gmail.com>
2021-03-30 10:57:18 -1000

    Add files via upload
    I need help comparing the ISBN of a stored value to a newly entered ISBN so that the program can tell the user if that book is already stored...i.e. if ISBN 12345 Title Happy is stored and someone tries to store ISBN 12345 Title Angry It needs to return ISBN 12345 Title Happy already exist.

---
+++ Book.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7779dc857fc75ee3e76911ba4be4a1d8e1ee3a7487bbaa6b7e3424857eee7f4
+size 2095
---
+++ Inventory.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3649e60830c0fe494467a134043c4edff07fea7e1594fc9624d7acd9cf9ca572
+size 5572
